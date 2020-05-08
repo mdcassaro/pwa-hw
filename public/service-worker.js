0ea680e55eb22a25cfc493bf10bc7a9a7df8e71b
Michael Cassaro <mdcassaro@hotmail.com>
2020-05-08 13:07:10 -0500

    gitignore added

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
 var CACHE_NAME = "my-site-cache-v1";
-const DATA_CACHE_NAME = "data-cache-v1";
+const FILES_TO_CACHE  = "data-cache-v1";
 
 var urlsToCache = [
   "/",
@@ -16,7 +16,7 @@ self.addEventListener("install", function(event) {
     event.waitUntil(
       caches.open(CACHE_NAME).then(function(cache) {
         console.log("Opened cache");
-        return cache.addAll(urlsToCache);
+        return cache.addAll(FILES_TO_CACHE );
       })
     );
   });
