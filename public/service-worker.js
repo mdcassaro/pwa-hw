7c0f8b32bb4eb7654352d0d69bfec1eb689b63ff
Michael Cassaro <mdcassaro@hotmail.com>
2020-05-08 14:41:03 -0500

    service page finished

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,8 @@
 var CACHE_NAME = "my-site-cache-v1";
-const FILES_TO_CACHE  = "data-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
 
-var urlsToCache = [
+var FILES_TO_CACHE  = [
   "/",
-  "/db.js",
   "/index.js",
   "/manifest.json",
   "/styles.css",
@@ -11,12 +10,47 @@ var urlsToCache = [
   "/icons/icon-512x512.png"
 ];
 
-//this installs the service worker
-self.addEventListener("install", function(event) {
-    event.waitUntil(
+
+self.addEventListener("install", function(evt) {
+    evt.waitUntil(
       caches.open(CACHE_NAME).then(function(cache) {
         console.log("Opened cache");
         return cache.addAll(FILES_TO_CACHE );
       })
     );
   });
+
+
+  self.addEventListener("fetch", function(evt) {
+    if (evt.request.url.includes("/api/")) {
+      evt.respondWith(
+        caches.open(DATA_CACHE_NAME).then(cache => {
+          return fetch(evt.request)
+            .then(response => {
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+  
+              return response;
+            })
+            .catch(err => {
+              return cache.match(evt.request);
+            });
+        }).catch(err => console.log(err))
+      );
+  
+      return;
+    }
+    evt.respondWith(
+        fetch(evt.request).catch(function() {
+          return caches.match(evt.request).then(function(response) {
+            if (response) {
+              return response;
+            } else if (evt.request.headers.get("accept").includes("text/html")) {
+              // return the cached home page for all requests for html pages
+              return caches.match("/");
+            }
+          });
+        })
+      );
+    });
